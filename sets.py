b9bdd8a1563cefbc7b2d863d08c93baedaabfb8e
mdbashironline <mdbashironline@gmail.com>
2025-08-09 20:14:53 +0530

    Program on set

---
+++ sets.py
@@ -0,0 +1,17 @@
+s = set()
+# Add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.add(6)
+print(s)
+
+s.remove(5)
+print(s)
+
+print(f"The length of the set is {len(s)}")
+
+
+
