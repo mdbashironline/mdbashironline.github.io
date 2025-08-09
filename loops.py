c723f96b1f1940f3ba92d9ea8ba6924b2400bbba
mdbashironline <mdbashironline@gmail.com>
2025-08-09 09:07:45 +0530

    Added loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+# Print for loop
+for i in [0,1,2,3,4,5,6,7,8,9]:
+    print(i)     
+
+for i in range(10):
+    print(i)
+    
+names = ['Alice', 'Bob', 'Charlie', 'David']  
+for name in names:
+    print(name)
+    
+names = "Harry"
+for letter in names:
+    print(letter)
+    
+    
