0b9107befae7b986f32300f357030e462c34246c
mdbashironline <mdbashironline@gmail.com>
2025-08-09 10:28:05 +0530

    Added new Python file

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+houses = {"Equality": "[Atika Ruhi, Anika Rida]","Freedom": "Inaaya Aira","Peace": "Ziana Aamra"}
+
+houses["Hermes"] = "Ifza Mohammad"
+print(houses)
+print(houses["Equality"])
+print(houses["Freedom"])
+print(houses["Peace"])  
+print(houses["Hermes"])
+
+
+
---
+++ functions.py
@@ -0,0 +1,8 @@
+def square(x):
+    """Returns the square of x."""
+    return x * x
+print(square(10))  # Example usage
+
+for i in range (11):
+    print(f"The square of {i} is {square(i)}")
+
